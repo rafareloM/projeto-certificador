6bac504526df01b7addf5ec45e9fc19ba3decb3c
leonardo <leonardo@multixsistemas.com.br>
2022-09-06 22:00:23 -0300

    init commit

---
+++ bin/projeto_certificador.dart
@@ -0,0 +1,5 @@
+import 'package:projeto_certificador/projeto_certificador.dart' as projeto_certificador;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto_certificador.calculate()}!');
+}
---
+++ test/projeto_certificador_test.dart
@@ -0,0 +1,8 @@
+import 'package:projeto_certificador/projeto_certificador.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
